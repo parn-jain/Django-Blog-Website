1afcd23ae92802f648ad06b6da09dcbe252f7394
“parn” <pjisvgreat@gmail.com>
2024-09-06 10:43:55 +0530

    whitenoice inrequirnments

--- blog_website1/settings.py
+++ blog_website1/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nkjp&(e1gsr#skiy@_=ee(6!npmvy)9jgztc6plqtx@#8pnvmh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['.vercel.app']
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'localhost:8000', '.vercel.app']
