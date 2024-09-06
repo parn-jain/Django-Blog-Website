6869ea644475fc835c843f718eab10b4f1010961
“parn” <pjisvgreat@gmail.com>
2024-09-06 17:30:48 +0530

    true

--- blog_website1/settings.py
+++ blog_website1/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-nkjp&(e1gsr#skiy@_=ee(6!npmvy)9jgztc6plqtx@#8pnvmh
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['.vercel.app']
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'localhost:8000', '.vercel.app']
